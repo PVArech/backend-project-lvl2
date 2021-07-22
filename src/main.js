0aaada3118631ec6b54f5c1bb2314e61a2586d97
Viachaslau Paulau <pavlov_box@tut.by>
2021-07-22 18:31:17 +0300

    finished two step genDiff

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import program from 'commander';
 export default () => {
   program
     .version('0.0.1')
-    .description('Compares two configuration files and shows a difference.');
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1> <filepath2>')
+    .action((filepath1, filepath2, format) => {
+      console.log(filepath1, filepath2, format);
+    });
+
   program.parse(process.argv);
 
   if (process.argv.length === 2) {
