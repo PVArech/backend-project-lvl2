f89d43e56ca387b6d688ebf07ee653bba1f161c5
Вячеслав <83239146+PVArech@users.noreply.github.com>
2021-07-23 12:55:11 +0300

    Update compareFiles.js

--- src/compareFiles.js
+++ src/compareFiles.js
@@ -1,14 +1,20 @@
 import fs from 'fs';
 import _ from 'lodash';
 
-const readFiles = (fullPath) => fs.readFileSync(fullPath, 'utf-8');
+const getContent = (fullPath) => fs.readFileSync(fullPath, 'utf-8');
+const getObj = (fileData) => JSON.parse(fileData);
 
-export const compareFiles = (filePath1, filePath2) => {
-  const objOne = JSON.parse(readFiles(filePath1));
-  const objTwo = JSON.parse(readFiles(filePath2));
-  const keys = _.union(_.keys(objOne), _.keys(objTwo)).sort();
-
-  const result = keys.reduce((acc, key) => {
+export const compareFiles = (filePath1, filePath2, format) => {
+  const fileContent1 = getContent(filePath1);
+  const fileContent2 = getContent(filePath2);
+  const objOne = getObj(fileContent1);
+  const objTwo = getObj(fileContent2);
+  const keys1 = _.keys(objOne);
+  const keys2 = _.keys(objTwo);
+  const keys = _.union(keys1, keys2);
+  
+  const result = keys.sort()
+  .reduce((acc, key) => { 
     if (objOne[key] === objTwo[key]) return [...acc, `    ${key}: ${objTwo[key]}`];
     if (objOne[key] !== undefined) acc.push(`  - ${key}: ${objOne[key]}`);
     if (objTwo[key] !== undefined) acc.push(`  + ${key}: ${objTwo[key]}`);
