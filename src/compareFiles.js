8893125a3a7dcbf5a4c04191d65218bc57febc4e
Viachaslau Paulau <pavlov_box@tut.by>
2021-07-23 23:23:12 +0300

    finished four step genDiff

--- src/compareFiles.js
+++ src/compareFiles.js
@@ -6,18 +6,15 @@ const readFiles = (fullPath) => fs.readFileSync(fullPath, 'utf-8');
 export const compareFiles = (filePath1, filePath2) => {
   const objOne = JSON.parse(readFiles(filePath1));
   const objTwo = JSON.parse(readFiles(filePath2));
-  const keys1 = _.keys(objOne);
-  const keys2 = _.keys(objTwo);
-  const keys = _.union(keys1, keys2).sort();
+  const keys = _.union(_.keys(objOne), _.keys(objTwo)).sort();
 
   const result = keys.reduce((acc, key) => {
     if (objOne[key] === objTwo[key]) return [...acc, `    ${key}: ${objTwo[key]}`];
     if (objOne[key] !== undefined) acc.push(`  - ${key}: ${objOne[key]}`);
     if (objTwo[key] !== undefined) acc.push(`  + ${key}: ${objTwo[key]}`);
-
     return acc;
-  }, []);
-  result.unshift('{');
+  }, ['{']);
+  // result.unshift('{');
   result.push('}');
   return result.join('\n');
 };
