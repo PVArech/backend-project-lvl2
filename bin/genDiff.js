594be429a7a98fe78a832e6be5c5ded2b4b181d0
Viachaslau Paulau <pavlov_box@tut.by>
2021-07-27 23:09:48 +0300

    modified bin/genDiff.js and deleted src/main.js

--- bin/genDiff.js
+++ bin/genDiff.js
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
-import main from '../src/main.js';
+import program from 'commander';
+import compareFiles from '../src/compareFiles.js';
 
-main();
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filePath1> <filePath2>')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .action((filePath1, filePath2) => {
+    const options = program.opts();
+    console.log(compareFiles(filePath1, filePath2, options.format));
+  });
+
+program.parse(process.argv);
--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // import main from './src/main.js';
 import compareFiles from './src/compareFiles.js';
 
-// console.log(compareFiles('/home/user/backend-project-lvl2/__fixtures__/file1.json',
-// '/home/user/backend-project-lvl2/__fixtures__/file2.json', 'plain'));
 export default compareFiles;
