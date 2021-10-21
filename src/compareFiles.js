c11efd5c5fef6b9143aee92a42e5f80d92e1aa09
Viachaslau Paulau <pavlov_box@tut.by>
2021-10-21 19:11:30 +0300

    modified src/compareFiles.js

--- src/compareFiles.js
+++ src/compareFiles.js
@@ -9,10 +9,9 @@ const getExtension = (fullPath) => path.extname(fullPath);
 
 const makeDiff = (objOne, objTwo) => {
   const keys = _.sortBy(_.union(_.keys(objOne), _.keys(objTwo)));
-  // const keys = _.union(_.keys(objOne), _.keys(objTwo)).sort();
 
   return keys.map((key) => {
-    if (_.isObject(objOne[key]) && _.isObject(objTwo[key])) {
+    if (_.isPlainObject(objOne[key]) && _.isPlainObject(objTwo[key])) {
       return { name: key, status: 'object', children: makeDiff(objOne[key], objTwo[key]) };
     }
     if (!_.has(objOne, key)) {
@@ -21,17 +20,17 @@ const makeDiff = (objOne, objTwo) => {
     if (!_.has(objTwo, key)) {
       return { name: key, status: 'deleted', value: objOne[key] };
     }
-    if (objOne[key] !== objTwo[key]) {
-      return {
-        name: key, status: 'changed', valueOne: objOne[key], valueTwo: objTwo[key],
-      };
+    if (_.isEqual(objOne[key], objTwo[key])) {
+      return { name: key, status: 'unchanged', value: objOne[key] };
     }
-    return { name: key, status: 'unchanged', value: objOne[key] };
+
+    return {
+      name: key, status: 'changed', valueOne: objOne[key], valueTwo: objTwo[key],
+    };
   });
 };
 
 const compareFiles = (filePath1, filePath2, format = 'stylish') => {
-// const compareFiles = (filePath1, filePath2, format) => {
   const objOne = parsers(getContent(filePath1), getExtension(filePath1));
   const objTwo = parsers(getContent(filePath2), getExtension(filePath2));
   const diff = makeDiff(objOne, objTwo);
